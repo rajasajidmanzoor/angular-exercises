4c08b50ab453a0c557ecdf452e318cdb8c70bf91
Raja Sajid Manzoor <sajidraja1@yahoo.com>
2025-06-24 19:09:15 +0500

    Formsubmit and reading values using Template variables and ViewChild

--- angular-concepts/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ angular-concepts/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  @ViewChild('form') form?:ElementRef<HTMLFormElement>; 
 
+  onSubmit(title: HTMLInputElement, ticketText: HTMLTextAreaElement) {
+
+    console.dir(title.value);
+    console.dir(ticketText.value);
+    title.classList.add('success');
+
+    this.form?.nativeElement.reset();
+
+  }
 }
