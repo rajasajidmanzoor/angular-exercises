a1f6c11e9f63c4251b0d8506bc19ae6d71940af1
Raja Sajid Manzoor <sajidraja1@yahoo.com>
2025-06-25 17:33:00 +0500

    signals, outputs and form submission all together

--- angular-concepts/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ angular-concepts/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { Component, ElementRef, output, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -16,13 +16,17 @@ export class NewTicketComponent {
   // Using Signal
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
+  add = output<{title: string, text: string }>();
+
   onSubmit(title: HTMLInputElement, ticketText: HTMLTextAreaElement) {
 
-    console.dir(title.value);
-    console.dir(ticketText.value);
-    title.classList.add('success');
+    // console.dir(title.value);
+    // console.dir(ticketText.value);
+    // title.classList.add('success');
 
+    this.add.emit({title: title.value, text: ticketText.value});
     this.form()?.nativeElement.reset();
+
 
   }
 }
--- angular-concepts/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ angular-concepts/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, signal, output } from '@angular/core';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+
+  data = input.required<Ticket>();
+  close = output();
+  detailsVisible = signal<boolean>(false);
+
+  onToggleDetails () {
+    this.detailsVisible.set(!this.detailsVisible()!);
+  }
+
+  onCloseTicket () {
+    this.close.emit();
+  }
 
 }
