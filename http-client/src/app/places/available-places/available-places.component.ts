fde3ce1e089d80279be3e2e0363bab5ee896abd0
Raja Sajid Manzoor <sajidraja1@yahoo.com>
2025-07-01 19:29:55 +0500

    Getting Results using HttpClient and showing data or error messages.

--- http-client/src/app/places/available-places/available-places.component.ts
+++ http-client/src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +13,37 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit{
+  isFetching = signal(false);
+  error = signal('');
   places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+
+  ngOnInit() {
+    console.log('Before Sending Request');
+    console.log('isFetching'+ this.isFetching());
+
+    this.isFetching.set(true);
+    const sub = this.httpClient.get<{ places: Place[]}>('http://localhost:3000/places').pipe(
+      map((resData)=> resData.places)
+    ).subscribe({
+      next: (places)=>{
+        this.places.set(places);
+        
+      },
+      complete: ()=>{
+        this.isFetching.set(false);
+        console.log('After Sub Destroyed1');
+      },
+      error : (error)=>{
+        this.error.set(error.message);
+      }
+    });
+
+    this.destroyRef.onDestroy(()=>{
+      sub.unsubscribe();
+    });
+  }
 }
