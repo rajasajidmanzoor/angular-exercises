fb41996502945deb6811c9e4f5a2c095de73eb15
Raja Sajid Manzoor <sajidraja1@yahoo.com>
2025-07-03 22:19:37 +0500

    Template Driven Forms with validation and local storage

--- forms/src/app/auth/login/login.component.ts
+++ forms/src/app/auth/login/login.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [FormsModule]
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(()=>{
+      const sub = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
+        next: (value)=> window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.email}))
+      });
+
+      this.destroyRef.onDestroy(()=> {
+        sub?.unsubscribe();
+      });
+    });
+  }
+
+
+  onSubmit(formData: NgForm){
+
+    if(formData.form.invalid){
+      return;
+    }
+    const enteredEmail = formData.form.value.email;
+    const enteredPassword = formData.form.value.password;
+
+    console.log(enteredEmail, enteredPassword);
+
+    formData.form.reset();
+  }
+}
